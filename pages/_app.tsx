5c0740b0ff13bd26566635e99acf058db08ad5fa
Simon maribo <64478800+Simonmaribo@users.noreply.github.com>
2022-06-09 18:08:45 +0200

    Added dayjs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,19 @@
 import { MantineProvider } from '@mantine/core';
 import { NotificationsProvider } from '@mantine/notifications';
-
 import '../styles/globals.css';
-
 import type { AppProps } from 'next/app'
 
+// DaysJS
+import dayjs from 'dayjs';
+require('dayjs/locale/en')
+
+var relativeTime = require('dayjs/plugin/relativeTime')
+dayjs.extend(relativeTime)
+dayjs.locale('en')
+
 function MyApp({ Component, pageProps }: AppProps) {
+
+
   return (
     <MantineProvider withNormalizeCSS>
       <NotificationsProvider>
