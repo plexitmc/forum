648ff9e1e84ad2e691d7d96d58293bd77db58300
Simon maribo <64478800+Simonmaribo@users.noreply.github.com>
2022-06-16 19:50:34 +0200

    changed application view header

--- components/pages/application/ApplicationInfo.tsx
+++ components/pages/application/ApplicationInfo.tsx
@@ -1,36 +1,51 @@
-import { ActionIcon, Anchor, Avatar, Box, Text, ThemeIcon, Tooltip } from "@mantine/core";
+import { Anchor, Avatar, Box, Text, ThemeIcon } from "@mantine/core";
 import dayjs from "dayjs";
 import Link from "next/link";
-import { FaCrown } from "react-icons/fa";
-import RoleBadge from "../../elements/RoleBadge";
 import StatusBadge from "../../elements/StatusBadge";
 import Application from "../../types/application";
 import Form from "../../types/form";
 import User from "../../types/user";
+import { ImClock } from 'react-icons/im';
+import { VscSmiley } from 'react-icons/vsc';
 
 export default function ApplicationInfo({ user, application, form }: { user: User, application: Application, form: Form }) {
     return (
         <Box>
-            <Box sx={{ display: 'flex', alignItems: 'center', gap: '0.3rem', justifyContent: 'center'}}>
-                {form.icon &&
-                    <ThemeIcon variant="light" size={40}>
-                        <i className={`${form.icon}`} style={{fontSize: '28px'}}/>
-                    </ThemeIcon>
-                }
-                <Text weight={600}>{form.name}</Text>
+            <Box sx={{ display: 'flex', justifyContent: 'center', marginBottom: '1rem'}}>
+                <Text weight={600} sx={{ fontSize: 45 }}>{form.name} application</Text>
             </Box>
-            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                <Text weight={600}>Date: <Text component="span" weight={400}>{dayjs(application.createdAt).format('HH:mm, DD/MM/YYYY')}</Text></Text>
-                <Text weight={600}>Status: <StatusBadge status={application.status}/></Text>
-                <Box sx={{ display: 'flex', alignItems: 'center', gap: '0.3rem'}}>
-                    <Text weight={600}>User: </Text>
+            <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', gap: '1rem' }}>
+                <Box sx={{ display: 'flex', alignItems: 'center', gap: '0.5rem'}}>
                     <Anchor component={Link} href={`/user/${user.id}`}>
-                        <Box sx={{ display: 'flex', gap: '0.1rem', cursor: 'pointer' }}>
+                        <Box sx={{ display: 'flex', gap: '0.5rem', cursor: 'pointer' }}>
                             <Avatar src={user.avatar} radius="xl" size={'sm'}/>
-                            <Text weight={400}>{user.username}</Text>
+                            <Text weight={600}>{user.username}</Text>
                         </Box>
                     </Anchor>
                 </Box>
+                <Box sx={{ display: 'flex', alignItems: 'center', gap: '0.5rem'}}>
+                    <ThemeIcon variant="light" size={30}>
+                        <ImClock size={18}/>
+                    </ThemeIcon>
+                    <Text weight={600}>{dayjs(application.createdAt).format('HH:mm, DD/MM/YYYY')}</Text>
+                </Box>
+                <Box sx={{ display: 'flex', alignItems: 'center', gap: '0.5rem'}}>
+                    <ThemeIcon variant="light" size={30}>
+                        <VscSmiley size={18}/>
+                    </ThemeIcon>
+                    <StatusBadge status={application.status}/>
+                </Box>
+                {/*
+                    <Box sx={{ display: 'flex', alignItems: 'center', gap: '0.3rem'}}>
+                        <Text weight={600}>User: </Text>
+                        <Anchor component={Link} href={`/user/${user.id}`}>
+                            <Box sx={{ display: 'flex', gap: '0.1rem', cursor: 'pointer' }}>
+                                <Avatar src={user.avatar} radius="xl" size={'sm'}/>
+                                <Text weight={400}>{user.username}</Text>
+                            </Box>
+                        </Anchor>
+                    </Box>
+                */}
             </Box>
         </Box>
     )
