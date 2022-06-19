a3e1a18e56bb8411576f0a77c8f937337bd206bd
Simon maribo <64478800+Simonmaribo@users.noreply.github.com>
2022-06-19 15:22:53 +0200

    Backend webhooks

---
+++ routes/webhooks/index.js
@@ -0,0 +1,56 @@
+// Imports
+const router = require('express').Router();
+
+// Export route
+module.exports = (db) => {
+    
+    // Utils
+    const webhooks = require('../../server/webhooks')(db);
+
+    // Create rate limiter
+    const rateLimiter = require('../../server/utils/rateLimiter')(2 * 60 * 1000, 15, "Too many requests. Try again later.")
+
+
+    // Get all webhooks
+    router.get('/', rateLimiter, async (req, res) => {
+        if(req.user.role !== 'admin') return res.status(403).send({ status: 403, message: 'You are not authorized to access this resource.' });
+        var webhooksArr = await webhooks.getWebhooks();
+        return res.status(200).send({ status: 200, message: "Webhooks retrieved.", webhooks: webhooksArr });
+    });
+
+    // Get a webhook by id
+    router.get('/:id', rateLimiter, async (req, res) => {
+        if(req.user.role !== 'admin') return res.status(403).send({ status: 403, message: 'You are not authorized to access this resource.' });
+        var webhook = await webhooks.getWebhookById(req.params.id);
+        if(!webhook) return res.status(404).send({ status: 404, message: 'Webhook not found.' });
+        return res.status(200).send({ status: 200, message: "Webhook retrieved.", webhook: webhook });
+    });
+
+
+    // Create webhook
+    router.post('/', rateLimiter, auth.ensureAuthenticationWithUser, async (req, res) => {
+        const { url, name, type } = req.body;
+        if(!url || !name || !type) return res.status(400).json({ status: 400, message: 'Missing required fields.' });
+
+        if(req.user.role !== 'admin') return res.status(403).json({ status: 403, message: 'You are not authorized to perform this action.' });
+
+        await webhooks.createWebhook(url, name, type, (response) => {
+            return res.status(response.status).json(response);
+        });
+    })
+
+    // Delete webhook
+    router.delete('/:id', rateLimiter, auth.ensureAuthenticationWithUser, async (req, res) => {
+        const { id } = req.params;
+        if(!id) return res.status(400).json({ status: 400, message: 'Missing required fields.' });
+
+        if(req.user.role !== 'admin') return res.status(403).json({ status: 403, message: 'You are not authorized to perform this action.' });
+
+        await webhooks.deleteWebhook(id, (response) => {
+            return res.status(response.status).json(response);
+        });
+    });
+
+
+    return router;
+}
---
+++ server/webhooks.js
@@ -0,0 +1,115 @@
+const { ObjectId } = require('mongodb');
+const { uuid } = require('uuidv4');
+const axios = require('axios')
+
+// Return module
+module.exports = (db) => {
+    const obj = {};
+
+    /**
+     * Creates a webhook for a channel
+     * @param {string} url - The url to send the webhook to
+     * @param {string} name - The name of the webhook
+     * @param {string} type - type of the webhook
+     * @param {Function} callback - Callback function
+     */
+    obj.createWebhook = async (url, name, type, callback) => {
+        try {
+            var secretKey = uuid().toString();
+
+            await db.webhooks.insertOne({
+                url: url,
+                name: name,
+                type: type,
+                secret: secretKey
+            }, (err, res) => {
+                if(err){
+                    console.log(err);
+                    return callback({ status: 500, message: 'An internal error occurred.' });
+                }
+                return callback({ status: 200, message: "Webhook has been created.", secret: secretKey })
+            });
+        } catch(err) {
+            console.log(err);
+            return callback({ status: 500, message: 'An internal error occurred.' });
+        }
+    }
+
+    /**
+     * Delete a webhook by id
+     * @param {string} id - The id of the webhook
+     * @param {Function} callback - Callback function
+     */
+    obj.deleteWebhook = async (id, callback) => {
+        try {
+            await db.webhooks.deleteOne({ _id: new ObjectId(id) }, (err, res) => {
+                if(err){
+                    console.log(err);
+                    return callback({ status: 500, message: 'An internal error occurred.' });
+                }
+                return callback({ status: 200, message: "Webhook has been deleted." });
+            });
+        } catch(err) {
+            console.log(err);
+            return callback({ status: 500, message: 'An internal error occurred.' });
+        }
+    }
+
+
+    /**
+     * Gets all webhooks
+     * @returns {Array} - Array of webhooks
+     */
+    obj.getWebhooks = async () => {
+        var webhooks = await db.webhooks.find({}).toArray();
+        return webhooks;
+    }
+
+    /**
+     * Gets a webhook by id
+     * @param {string} id - The id of the webhook
+     * @returns {Object} - The webhook
+     */
+    obj.getWebhookById = async (id) => {
+        try {
+            var webhook = await db.webhooks.findOne({ _id: new ObjectId(id) });
+            return webhook;
+        } catch(err) {
+            console.log(err);
+            return null;
+        }
+    }
+
+    /**
+     * Send request to webhooks with data
+     * @param {string} type - The type of the webhook
+     * @param {Object} data - The data to send
+     */
+    obj.sendWebhook = async (type, data) => {
+        try {
+            var webhooks = await db.webhooks.find({ type: type }).toArray();
+            for(var i = 0; i < webhooks.length; i++){
+                var webhook = webhooks[i];
+                await axios.post(webhook.url, { type: type, secret: webhook.secretKey, data: data }, { headers: { 'Content-Type': 'application/json' } });
+            }
+        } catch(err) {
+            console.log(err);
+        }
+    }
+    
+
+    /**
+     * Send webhook when a comment is created.
+     * @param {*} application - The application which the comment was created on
+     * @param {*} comment - The comment which was created
+     */
+    obj.onComment = async (application, comment) => {
+        var data = {
+            application: application,
+            comment: comment
+        }
+        await obj.sendWebhook('comment', data);
+    }
+
+    return obj;
+}
